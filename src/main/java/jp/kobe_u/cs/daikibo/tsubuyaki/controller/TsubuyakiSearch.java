b056d5034e996687f5231113fa014f8c83796605
S-ikegami-2023 <234t202t@gsuite.kobe-u.ac.jp>
2023-07-24 00:17:02 +0900

    つぶやきウェブアプリ追加機能実装

---
+++ src/main/java/jp/kobe_u/cs/daikibo/tsubuyaki/controller/TsubuyakiSearch.java
@@ -0,0 +1,13 @@
+package jp.kobe_u.cs.daikibo.tsubuyaki.controller;
+
+public class TsubuyakiSearch {
+    private String search;
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+}
